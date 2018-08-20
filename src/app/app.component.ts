67a09e5163eb183679f8133db2f4d64f2397cdc6
Edgar Diaz <diaz_eda@hotmail.com>
2018-08-19 23:31:14 -0500

    Agregando servicio para la comunicación con los Web Services.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,8 +8,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Proyecto mineria de datos Random Forest';
-
-  findForest() {
-    console.log('Consultar algoritmo random forest');
+  resultExactitud;
+  resultModelo;
+  exactitud() {
+    // Consulta el servicio web para obtener la exactitud
+    this.findExactitud()
+    .subscribe((response: any) => {
+      console.log(response);
+      this.resultExactitud = response;
+    });
   }
+
+  modelo() {
+    // Consulta el servicio web para obtener la exactitud
+    this.findModelo()
+    .subscribe((response: any) => {
+      console.log(response);
+      this.resultModelo = response;
+    });
+  }
+
+
+  getQuery(query: string) {
+    const url = `http://localhost:8080/forest/${query}`;
+    console.log(url);
+    return this.http.get(url);
+  }
+
+  constructor(private http: HttpClient) { }
+  
+  findExactitud() {
+    return this.getQuery('exactitud/');
+  }
+
+  findModelo() {
+    return this.getQuery('modelo/');
+  }
+
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {ButtonModule} from 'primeng/button';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 
@@ -9,7 +10,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule, ButtonModule
+    BrowserModule, ButtonModule, HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
